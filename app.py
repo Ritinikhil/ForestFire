a0f114ebac8603bd98c450dfc5935c26dfb5c482
Ritinikhil <124866156+Ritinikhil@users.noreply.github.com>
2025-03-03 09:35:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def hello_world():
+    return render_template('forest_fire.html')  # Just the template name, not full path
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    try:
+        int_features = [float(x) for x in request.form.values()]  # Changed to float for decimal values
+        final = [np.array(int_features)]
+        print(int_features)
+        print(final)
+        prediction = model.predict_proba(final)
+        output = '{0:.{1}f}'.format(prediction[0][1], 2)
+
+        if float(output) > 0.5:
+            return render_template('forest_fire.html',
+                                pred='Your Forest is in Danger.\nProbability of fire occurring is {}'.format(output),
+                                bhai="Forest is in Danger.")
+        else:
+            return render_template('forest_fire.html',
+                                pred='Your Forest is safe.\nProbability of fire occurring is {}'.format(output),
+                                bhai="Your Forest is Safe for now")
+    except Exception as e:
+        return render_template('forest_fire.html',
+                             pred='An error occurred: {}'.format(str(e)))
+
+if __name__ == '__main__':
+    app.run(debug=True)
